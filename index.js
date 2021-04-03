02eecf41bab34539b1555bf7f59c7dae2a72f299
Janeé Mays <janee.elise@gmail.com>
2021-04-03 18:14:53 -0400

    added commented html

--- index.js
+++ index.js
@@ -104,3 +104,25 @@ choices: ["Engineer", "Intern", "I don't want to add any more team members."],
 },
 ])
 }
+// const generateHTML = (answers) =>
+//     `<!DOCTYPE html>
+// <html lang="en">
+// <head>
+//   <meta charset="UTF-8">
+//   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+//   <title>Document</title>
+// </head>
+// <body>
+//   <div class="jumbotron jumbotron-fluid style = color:purple;">My Team</div>
+//   <div class="container">
+//     <h1 class="display-4">${answers.name}</h1>
+//     <p class="lead">ID: ${answers.location}.</p>
+//     <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+//     <ul class="list-group">
+//       <li class="list-group-item">My GitHub username is ${answers.github}</li>
+//       <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
+//     </ul>
+//   </div>
+// </body>
+// </html>`;
